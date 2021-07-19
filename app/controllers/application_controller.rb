ab7dc08011536752814dfc7793b8c48c35979c25
Hunter <fleakhunter@gmail.com>
2021-07-19 19:18:26 -0400

    Refactor flash message helper into Application Controller.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::Base
+    def flash_message
+        if flash[:notice] || flash[:alert] 
+            content_tag(:div, flash[:notice] || flash[:alert], class: "alert alert-#{flash[:notice] ? 'success' : 'danger'} alert dismissable fade show m-0", role:'alert')
+        end   
+    end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,2 @@
 module ApplicationHelper
-    def flash_message
-        if flash[:notice] || flash[:alert] 
-            content_tag(:div, flash[:notice] || flash[:alert], class: "alert alert-#{flash[:notice] ? 'success' : 'danger'} alert dismissable fade show m-0", role:'alert')
-        end   
-    end
 end
