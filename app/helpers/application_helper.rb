19d83230db032f7af6a48c43929d56ab565c7ab8
Hunter <fleakhunter@gmail.com>
2021-07-19 18:09:30 -0400

    Add flash message helper method.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+    def flash_message
+        if flash[:notice] || flash[:alert] 
+            content_tag(:div, flash[:notice] || flash[:alert], class: "alert alert-#{flash[:notice] ? 'success' : 'danger'} alert dismissable fade show m-0", role:'alert')
+        end   
+    end
 end
