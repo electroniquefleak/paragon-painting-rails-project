f8d10d5a92c18196f7c6bcce0d3509e2919fd1af
Hunter <fleakhunter@gmail.com>
2021-07-19 19:21:58 -0400

    Create current_user helper.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,12 @@ class ApplicationController < ActionController::Base
             content_tag(:div, flash[:notice] || flash[:alert], class: "alert alert-#{flash[:notice] ? 'success' : 'danger'} alert dismissable fade show m-0", role:'alert')
         end   
     end
+
+    def current_user
+        if session[:user_id]
+            @current_user ||= User.find(session[:user_id])
+        else
+            @current_user = nil
+        end
+    end
 end
