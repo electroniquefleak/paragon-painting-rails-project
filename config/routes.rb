a294e565eb8efae4511bbddf41f76b9adf41bced
Hunter <fleakhunter@gmail.com>
2021-07-19 19:07:31 -0400

    Create Home Controller, welcome action, and route that displays landing view.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :users, only: [:new, :create]
   resources :sessions, only: [:create, :destroy]
-
+  
+  root to: 'home#welcome'
   get "login", to: 'sessions#new', as: "login"
 end
