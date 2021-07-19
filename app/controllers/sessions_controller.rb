e465761026a0d679b4a8f18c8ef3881664f94460
Hunter <fleakhunter@gmail.com>
2021-07-19 19:14:18 -0400

    Refactor redirect in User create action; create Sessions destroy action (logout feature).

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,10 @@
 class SessionsController < ApplicationController
     def new
+        @user = User.new
+    end
+
+    def destroy
+        session.clear
+        redirect_to login_path
     end
 end
