366301b770834ba4f9ae73540ef39717525f627f
Hunter <fleakhunter@gmail.com>
2021-07-21 19:32:05 -0400

    Refactor dashboard view with admin/employee partials; add require_login before action to dashboard.

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,4 +1,15 @@
 class DashboardsController < ApplicationController
+  before_action :require_login
+  
   def dashboard
+  end
+
+  private
+
+  def require_login
+    unless current_user
+    flash[:alert] = "You must be logged in to access this section."
+    redirect_to login_path
+    end
   end
 end
