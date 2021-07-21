a7fa150f6b15724632867a8f9ab4b2c504132d62
Hunter <fleakhunter@gmail.com>
2021-07-21 19:08:05 -0400

    Add create action in Sessions Controller; removed errors partial from new view.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,20 @@
 class SessionsController < ApplicationController
     def new
-        @user = User.new
+    end
+
+    def create
+        user = User.find_by_email(params[:email])
+        if user && user.authenticate(params[:password])
+          session[:user_id] = user.id
+          redirect_to root_path, notice: "You are logged in!"
+        else
+          if user == nil
+            flash[:alert] = "Invalid email.  Please try again."
+          else
+            flash[:alert] = "Invalid password."
+          end
+          render :new
+        end
     end
 
     def destroy
