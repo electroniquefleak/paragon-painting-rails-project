6f7db5ffe8e4c18d359cda0be8482be292f7be59
Hunter <fleakhunter@gmail.com>
2021-07-22 10:53:26 -0400

    Add create action for Project.

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -2,4 +2,14 @@ class ProjectsController < ApplicationController
     def new
         @project = Project.new
     end
+
+    def create
+        @project = Project.new
+        if @project.save
+          flash[:notice] = "Your project has been created."
+          redirect_to dashboard_path
+        else 
+          render :new
+        end
+    end
 end
