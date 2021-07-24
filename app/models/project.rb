e1c5ce286b0caebe3cccbdd9d7424d048377d644
Hunter <fleakhunter@gmail.com>
2021-07-23 20:29:46 -0400

    Hotfix in Project relationship association; create link to add team on Project show.

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,4 @@
 class Project < ApplicationRecord
     has_many :teams, :dependent => :destroy
-    has_many :users, through :teams, :dependent => :destroy
+    has_many :users, through: :teams, :dependent => :destroy
 end
