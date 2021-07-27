e5cd30e20bc9a1db5930c5f802a37826a2e8bdb4
Hunter <fleakhunter@gmail.com>
2021-07-27 13:14:26 -0400

    Add create action and set role params in Roles Controller; content copy updates on adashboard views; update link information on Project show.

--- app/controllers/roles_controller.rb
+++ app/controllers/roles_controller.rb
@@ -3,4 +3,30 @@ class RolesController < ApplicationController
         @role = Role.new
         @project = Project.find(params[:project_id])
     end
+
+    def create
+        @role = Role.new(role_params)
+        @role.project_id = params[:project_id]
+        if @role.save
+            flash[:notice] = 'Teammate Added!'
+            redirect_to project_role_path(params[:project_id])
+        else
+            flash[:error] = 'Failed to add teammate!'  
+            render :new
+        end
+    end
+
+    def show
+    end
+
+
+
+    private
+
+    def role_params
+        params.require(:role).permit(
+            :title,
+            :user_id
+          )
+    end
 end
