23c838e2321ff761fd141186c5e43c7c3c81fa7c
Hunter <fleakhunter@gmail.com>
2021-07-28 14:43:18 -0400

    Create scope methods for active, future, and past projects.

--- app/models/project.rb
+++ app/models/project.rb
@@ -3,4 +3,8 @@ class Project < ApplicationRecord
     has_many :users, through: :roles, :dependent => :destroy
 
     accepts_nested_attributes_for :roles, reject_if: :all_blank
+
+    scope :active, -> { where("? BETWEEN start_date AND end_date", Date.today)} 
+    scope :past, -> { where('end_date < ?', Date.today)} 
+    scope :future, -> { where('start_date > ?', Date.today)}
 end
